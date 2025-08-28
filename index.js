8043157d67392ce5cef231374909e3b92529bf49
smziaulmillat <asique1104@gmail.com>
2025-08-28 12:14:38 +0600

    added emojis and copying number function and responsiveness in three media and alert functionality

--- index.js
+++ index.js
@@ -29,31 +29,68 @@ for(const call of calls){
         const title = call.parentNode.parentNode.children[1].innerText;
         const number = call.parentNode.parentNode.children[3].innerText;
         const time = new Date().toLocaleTimeString()
-        // console.log(time);
-
+        
         alert("Calling "+ title +' '+number)
-
+        
         const newRealCoin = realCoinNum - 20;
         document.getElementById('real-coin').innerText = newRealCoin;
         
         const container = document.getElementById('card-container');
         const newDiv = document.createElement('div')
         newDiv.innerHTML= `
-        <div class="mt-5">
-                         <div class="flex items-center justify-between">
-                             <div class="">
-                                 <h4 class="text-[18px] font-bold w-44">${title}</h4>
-                                 <p class="text-[15px] text-gray-500">${number}</p>
-                             </div>
-                             <p class="text-[16px] text-gray-600">${time}</p>
-                         </div>
-
-
+        <div class="mt-5 shadow-sm">
+        <div class="flex items-center justify-between">
+        <div class="">
+        <h4 class="text-[18px] font-bold lg:w-44">${title}</h4>
+        <p class="text-[15px] text-gray-500">${number}</p>
+        </div>
+        <p class="text-[16px] text-gray-600">${time}</p>
+        </div>
+        
+        
         `
-
         container.appendChild(newDiv);
         
-
     })
-
+    
 }
+
+// clear functionality
+
+document.getElementById("clear-btn").addEventListener("click" , function(){
+    
+    const container = document.getElementById('card-container');
+    container.innerText = ''
+})
+
+// copy btn 1st part
+
+const copyBtns = document.getElementsByClassName("copy-btn")
+
+for(const btn of copyBtns){
+    btn.addEventListener("click" , function(){
+        
+        const number = btn.parentNode.parentNode.children[3].innerText;
+        alert('You are copying the number: ' + number)
+        
+        const realCopy = document.getElementById('copy-real-btn').innerText;
+        const newCopy = Number(realCopy) + 1;
+        document.getElementById('copy-real-btn').innerText = newCopy
+    })
+    
+}
+
+
+const btns = document.querySelectorAll('.copy-btn');
+
+// console.log(btn);
+for(const btn of btns){
+  btn.addEventListener('click', function() {
+
+    const card = btn.closest('.card');
+    const text = card.querySelector('.copy-text').textContent.trim();
+
+    navigator.clipboard.writeText(text);
+    // alert("Copied: " + text);
+  })
+  }
